9d1e2efe0c22fdbff0c3b144e39e9582bc8299a3
doncity-tech <princewill.opara@outlook.com>
2020-05-18 12:59:46 +0100

    all good

---
+++ react-todo/src/component/TodoFilter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoFilter = () => {
+  return (
+    <div>
+      <button>All</button>
+      <button>Completed</button>
+      <button>Incomplete</button>
+    </div>
+  );
+}
+
+export default TodoFilter
