9c410f42c47c19ef1c14f12be5db2a456cf84e4d
doncity-tech <princewill.opara@outlook.com>
2020-05-19 16:56:23 +0100

    Almost done

--- react-todo/src/app.js
+++ react-todo/src/app.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import TodoList from './component/TodoList'
-import AddTodo from './component/AddTodo'
 import TodoFilter from './component/TodoFilter'
 import './style.css'
 
 // function Component
-const Template = () => {
-  return (
-    <div className="container">
-      <div className="header">
-        <h1>React Todo List</h1>
+class Template extends React.Component {
+  state = {
+    input: '',
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <h1>React Todo List</h1>
+        </div>
+        <div>
+          <TodoList />
+        </div>
+        <div className="footer">
+          <TodoFilter />
+        </div>
       </div>
-      <div className="addtodo">
-        <AddTodo />
-      </div>
-      <div className="todolist">
-        <TodoList />
-      </div>
-      <div className="footer">
-        <TodoFilter />
-      </div>
-    </div>
-  )
+    )
+  }
+
 }
 class App extends React.Component {
   render() {
