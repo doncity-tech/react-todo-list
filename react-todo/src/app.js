9af9353ce6267985b46ffddc3d51ee998412905a
doncity-tech <princewill.opara@outlook.com>
2020-05-30 14:37:25 +0100

    update

--- react-todo/src/app.js
+++ react-todo/src/app.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import TodoList from './component/TodoList'
-import TodoFilter from './component/TodoFilter'
-import './style.css'
+import React from 'react';
+import TodoList from './component/TodoList';
+import TodoFilter from './component/TodoFilter';
+import './style.css';
 
 // function Component
-class Template extends React.Component {
-  state = {
-    input: '',
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="header">
-          <h1>React Todo List</h1>
-        </div>
-        <div>
-          <TodoList />
-        </div>
-        <div className="footer">
-          <TodoFilter />
-        </div>
-      </div>
-    )
-  }
-
+class Layout extends React.Component {
+	state = { input: '' };
+	render() {
+		return (
+			<div className='container'>
+				<div className='header'>
+					<h1>React Todo List</h1>
+				</div>
+				<div>
+					<TodoList />
+				</div>
+				<div className='footer'>
+					<TodoFilter />
+				</div>
+			</div>
+		);
+	}
 }
 class App extends React.Component {
-  render() {
-    return (<Template />);
-  };
+	render() {
+		return <Layout />;
+	}
 }
 
-export default App
+export default App;
--- react-todo/src/component/TodoList.js
+++ react-todo/src/component/TodoList.js
@@ -1,50 +1,63 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 class AddTodo extends React.Component {
-  state = { task: '' };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.task);
-    this.setState({ task: '' })
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text"
-          placeholder="Add Task"
-          onChange={e => this.setState({ task: e.target.value })}
-          value={this.state.task} required />
-        <button className="add-task">+Task</button>
-      </form>
-    );
-  }
+	state = { task: '' };
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.onSubmit(this.state.task);
+		this.setState({ task: '' });
+	};
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type='text'
+					placeholder='Add Task'
+					onChange={(e) => this.setState({ task: e.target.value })}
+					value={this.state.task}
+					required
+				/>
+				<button className='add-task'>+Task</button>
+			</form>
+		);
+	}
 }
 
 class TodoList extends React.Component {
-  state = {
-    tasks: []
-  }
-  addTodoHandler = (addedTask) => {
-    this.setState(prevState => ({
-      tasks: [...prevState.tasks, addedTask],
-    }))
-  }
+	state = {
+		tasks: [],
+	};
+	addTodoHandler = (addedTask) => {
+		this.setState((prevState) => ({
+			tasks: [
+				...prevState.tasks,
+				{ id: 'todo-0', task: addedTask, completed: false },
+			],
+		}));
+	};
 
-  render() {
-    return (
-      <div>
-        <div className="addtodo">
-          <AddTodo onSubmit={this.addTodoHandler} />
-        </div>
-        <div className="todolist">
-          <ul>
-            {this.state.tasks.map(list => <li>{list}</li>)}
-          </ul >
-        </div>
-      </div>
-    );
-  }
-
+	render() {
+		return (
+			<div>
+				<div className='addtodo'>
+					<AddTodo onSubmit={this.addTodoHandler} />
+				</div>
+				<div className='todolist'>
+					<ul>
+						{this.state.tasks.map((list) => (
+							<Todo
+								task={list.task}
+								completed={list.completed}
+								id={list.id}
+								key={list.id}
+							/>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default TodoList
+export default TodoList;
